a38e7558ff11d394e1b34fdea67b85cfe5a0f488
Denton Alford <dalford@workwithopal.com>
2020-04-29 16:13:34 -0700

    actually use the variables I made

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
-DOTFILES_ZSHRC="./.zshrc"
+DOTFILES_ZSHRC="$HOME/dotfiles/.zshrc"
 TARGET_ZSHRC="$HOME/.zshrc"
 
-echo -e '\n\n# Contents below copied from dotfiles .zshrc' >> TARGET_ZSHRC
-cat DOTFILES_ZSHRC >> TARGET_ZSHRC
-echo -e '\n# End .zshrc copy\n\n' >> TARGET_ZSHRC
-source TARGET_ZSHRC
+echo -e '\n\n# Contents below copied from dotfiles .zshrc' >> $TARGET_ZSHRC
+cat $DOTFILES_ZSHRC >> $TARGET_ZSHRC
+echo -e '\n# End .zshrc copy\n\n' >> $TARGET_ZSHRC
+source $TARGET_ZSHRC
