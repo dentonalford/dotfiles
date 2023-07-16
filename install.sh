f350bb5b65d6820df31cca76eb3356f9902dcbef
Denton Alford <alford235@gmail.com>
2023-07-15 23:19:03 -0700

    cleanup

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-TARGET_ZSHRC="$HOME/.zshrc"
-
-cat ./.zshrc >> $TARGET_ZSHRC
--- opal_embeditor_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DOTFILES_ZSHRC="$HOME/dotfiles/.zshrc"
-TARGET_ZSHRC="$HOME/.zshrc"
-
-cat $DOTFILES_ZSHRC >> $TARGET_ZSHRC
