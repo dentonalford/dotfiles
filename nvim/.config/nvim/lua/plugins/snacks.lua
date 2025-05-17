b501bdd868ecc85b5800b38c50a73141cb802e88
Denton Alford <alford235@gmail.com>
2025-05-16 17:11:11 -0700

    stub in lsp setup

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -13,10 +13,10 @@ return {
       -- refer to the configuration section below
       bigfile = { enabled = true },
       dashboard = { enabled = true },
-      explorer = { enabled = true },
+      explorer = { enabled = false },
       indent = { enabled = true },
       input = { enabled = true },
-      picker = { enabled = true },
+      picker = { enabled = false },
       notifier = { enabled = true },
       quickfile = { enabled = true },
       scope = { enabled = true },
