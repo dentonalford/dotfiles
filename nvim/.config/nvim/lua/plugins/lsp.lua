98318fcec80f2dfd7ba548e21e19ff3fc0cdf579
Denton Alford <dalford@workwithopal.com>
2025-07-03 09:29:03 -0700

    add elixir and typescript language servers to config

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -205,19 +205,7 @@ return {
         local servers = {
             bashls = {},
             clangd = {},
-            marksman = {},
-            -- gopls = {},
-            -- pyright = {},
-            -- rust_analyzer = {},
-            -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-            --
-            -- Some languages (like typescript) have entire language plugins that can be useful:
-            --    https://github.com/pmizio/typescript-tools.nvim
-            --
-            -- But for many setups, the LSP (`ts_ls`) will work just fine
-            -- ts_ls = {},
-            --
-
+            elixirls = {},
             lua_ls = {
                 -- cmd = { ... },
                 -- filetypes = { ... },
@@ -232,6 +220,8 @@ return {
                 --   },
                 -- },
             },
+            marksman = {},
+            ts_ls = {},
         }
 
         -- Ensure the servers and tools above are installed
