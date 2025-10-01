0c5a5a2fb6b86be25e0832b28dda7bb6da17bd7d
Denton Alford <dalford@workwithopal.com>
2025-10-01 10:52:32 -0500

    add blink-cmp and diffview

---
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,8 @@
+return {
+	"sindrets/diffview.nvim",
+	opts = {},
+	keys = {
+		{ "<leader>gw", "<cmd>DiffviewOpen<cr>", desc = "Diff [g]it [w]orking directory" },
+		{ "<leader>gW", "<cmd>DiffviewClose<cr>", desc = "Close Diffview" },
+	},
+}
